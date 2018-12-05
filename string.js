2609756261cf948835fcb0c05f7bfdaebd928ec5
Extrance <balasso.andrea69@gmail.com>
2018-12-05 12:29:05 +0100

    fix

---
+++ string.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const body = require('body-parser');
+const app = express();
+
+app.get('/square' , (req,res) => {
+  var s = req.query.string;
+  var a = {result: string_square(s)};
+  res.json(a)
+  res.end();
+});
+
+function string_square(s) {
+  try {
+    var len = s.length;
+    return len*len;
+  }
+  catch(e) {
+    return -1;
+  }
+}
+
+app.listen(4000 || process.env.PORT, () => console.log('App is online on port 4000'))
